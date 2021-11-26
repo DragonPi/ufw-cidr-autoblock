3b5f306bb5261cf61b1b2a21a667ae7a1a3f8ed4
DragonPi <86000094+DragonPi@users.noreply.github.com>
2021-11-26 21:32:34 +0100

    CLI setup (#5)

--- cmd/version.go
+++ cmd/version.go
@@ -13,11 +13,28 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
+package cmd
 
-import "github.com/DragonPi/ufw-cidr-autoblock/cmd"
+import (
+	"fmt"
 
-func main() {
-	c := cmd.New()
-	c.Execute()
+	"github.com/spf13/cobra"
+)
+
+func (c* Cmd) addVersionCmd() {
+	c.cmds["version"] = &cobra.Command{
+		Use:   "version",
+		Short: "Display application version",
+		Long:  "This command returns the version.",
+		Example: "lnx-database-tool version",
+		Args: cobra.ExactArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			c.printVersion()
+		},
+	}
+}
+
+// printVersion returns the version of the application
+func (c *Cmd) printVersion(){
+	fmt.Println("version called")
 }
